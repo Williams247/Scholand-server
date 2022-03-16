a2afb27cd34576e7d739a80488ee010ab1ee39d9
Williams Alex <williamsalexjr@gmail.com>
2022-03-16 09:19:28 +0100

    Deleted all the wanted files in the service folder

--- services/index.js
+++ services/index.js
@@ -1,9 +1,5 @@
-const Login = require("./login");
-const SendMailGenerateOTP = require("./send-mail-generate-otp");
 const Profile = require("./profile");
 
 module.exports = {
-  Login,
-  SendMailGenerateOTP,
   Profile
 };
